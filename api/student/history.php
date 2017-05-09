f9cf4401388935174772bb20a24ac20b6b1ef7e2
hieplq <lequanghiep74@gmail.com>
2017-05-09 16:09:49 +0700

    display list history of driver

--- api/student/history.php
+++ api/student/history.php
@@ -4,10 +4,10 @@ session_start();
 $db = new DB();
 $query = "select sq.id, sq.status, sq.route_queue_id, rq.queue, flc.name as from_location, tlc.name as to_location, b.plate_no, rq.start_time "
     . "from student_queue sq "
-    . "inner join route_queue rq on rq.id = sq.route_queue_id "
-    . "inner join location flc on flc.id = rq.from_location_id "
-    . "inner join location tlc on tlc.id = rq.to_location_id "
-    . "inner join bus b on b.id = rq.bus_id "
+    . "left join route_queue rq on rq.id = sq.route_queue_id "
+    . "left join location flc on flc.id = rq.from_location_id "
+    . "left join location tlc on tlc.id = rq.to_location_id "
+    . "left join bus b on b.id = rq.bus_id "
     . "where student_id = " . $_SESSION['user']['id'];
 $data = $db->query($query);
 if ($data) {
--- resource/js/driver/history.js
+++ resource/js/driver/history.js
@@ -1,12 +1,62 @@
-/**
- * Created by thuan on 3/20/2017.
- */
 $(document).ready(function () {
     $(".button-collapse").sideNav();
-    $('select').material_select();
-    $('.datepicker').pickadate({
-        selectMonths: true, // Creates a dropdown to control month
-        selectYears: 15, // Creates a dropdown of 15 years to control year
-        format:'dd/mm/yyyy'
-    });
+
+    var getHistory = function getHistory() {
+        $.ajax({
+            url: "/queue/api/driver/history.php",
+            dataType: "text",
+            type: 'get',
+            success: function (data) {
+                data = JSON.parse(data);
+                data.forEach(function (val) {
+                    $('#histories').append(genHistoryItem(val));
+                });
+            },
+            error: function (error) {
+                alert(error.responseText);
+            }
+        });
+    };
+
+    var genHistoryItem = function (data) {
+        var html = '<div class="row">'
+            + '<div class="col s12 m6 offset-m3 ">'
+            + '<div class="card-panel col s12 ">'
+            + '<div class="card-content col s10" style="padding:10px 0 10px 0">'
+            + '<span class="teal-text col s12 m12">'
+            + '<i class="material-icons s1">location_on</i>'
+            + '<i class="small"><b>From </b>' + data.from_location + ' </i>'
+            + '<i class="small"><b>To </b>' + data.to_location + '</i>'
+            + '</span><br>'
+            + '<span class="teal-text col s12 m12">'
+            + '<i class="material-icons">date_range</i>'
+            + '<i class="small"><b>Date </b>' + data.start_time.substr(0, data.start_time.lastIndexOf(' ')) + '</i>'
+            + '</span><br>'
+            + '<span class="teal-text col s12 m12">'
+            + '<i class="material-icons s1">watch_later</i>'
+            + '<i class="small"><b>Time </b> ' + data.start_time.substr(12, data.start_time.lastIndexOf(':') - 12) + '</i>'
+            + '</span><br>'
+            + '<span class="teal-text col s12 m12">'
+            + '<i class="material-icons s1">directions_bus</i>'
+            + '<i class="small"><b>Bus </b>' + data.plate_no + '</i>'
+            + '</span>'
+            + '</div>'
+            + '<div class="col s2 ">';
+        switch (data.status) {
+            case '0':
+                html += '<a class="btn-floating waves-effect waves-light blue lighten-1 center-element center-align">'
+                    + data.queue + '</a>';
+                break;
+            case '1':
+                html += '<a class="btn-floating waves-effect waves-light red lighten-1 center-element center-align">Close</a>';
+                break;
+            case '2':
+                html += '<a class="btn-floating waves-effect waves-light teal lighten-1 center-element center-align">Done</a>';
+                break;
+        }
+        html += ' </div></div></div></div>';
+        return html;
+    };
+
+    getHistory();
 });
