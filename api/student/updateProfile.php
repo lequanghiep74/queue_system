0d3b1c26a2015ac8d67950c2a8b07128f08f24a1
hieplq <lequanghiep74@gmail.com>
2017-05-11 00:40:11 +0700

    profile of student

---
+++ api/student/updateProfile.php
@@ -0,0 +1,52 @@
+<?php
+require "../include/DB.php";
+$db = new DB();
+if (isset($_GET['type'])
+    && isset($_GET['id'])
+) {
+    if ($_GET['type'] == 'info') {
+        if (isset($_GET['full_name'])
+            && isset($_GET['identity_id'])
+            && isset($_GET['day_of_birth'])
+            && isset($_GET['student_no'])
+            && isset($_GET['phone_number'])
+        ) {
+            $query = "update student set "
+                . "fullname = '" . $_GET['full_name'] . "', "
+                . "identity_id = '" . $_GET['identity_id'] . "', "
+                . "dob = STR_TO_DATE('" . $_GET['day_of_birth'] . "', '%d/%m/%Y'), "
+                . "student_no = '" . $_GET['student_no'] . "', "
+                . "phone = '" . $_GET['phone_number'] . "' "
+                . "where id = " . $_GET['id'];
+            if ($db->query($query) === true) {
+                header(' ', true, 200);
+            } else {
+                header(' ', true, 400);
+                echo 'Error!!!';
+            }
+        } else {
+            header('Missing data', true, 400);
+            echo 'Missing data';
+        }
+    } else {
+        if (isset($_GET['password'])) {
+            $query = "update student set "
+                . "password = '" . $_GET['password'] . "' "
+                . "where id = " . $_GET['id'];
+            echo $query;
+            if ($db->query($query) === true) {
+                header(' ', true, 200);
+            } else {
+                header(' ', true, 400);
+                echo 'Error!!!';
+            }
+        } else {
+            header('Missing data', true, 400);
+            echo 'Missing data';
+        }
+    }
+} else {
+    header('Missing data', true, 400);
+    echo 'Missing data';
+}
+?>
--- resource/js/student/profile.js
+++ resource/js/student/profile.js
@@ -1,12 +1,108 @@
-/**
- * Created by thuan on 4/2/2017.
- */
 $(document).ready(function () {
+    var tab = 'general';
+    var profile = {};
+    var user = JSON.parse(window.localStorage.getItem('user'));
     $(".button-collapse").sideNav();
-    $('select').material_select();
     $('.datepicker').pickadate({
         selectMonths: true, // Creates a dropdown to control month
         selectYears: 15, // Creates a dropdown of 15 years to control year
-        format:'dd/mm/yyyy'
+        format: 'dd/mm/yyyy'
     });
+
+    $('.tab').click(function () {
+        tab = $(this).attr('data');
+    });
+
+    $("#submit-btn").click(function () {
+        var obj = {};
+        var isUpdate = true;
+        if (tab === 'password') {
+            var new_pass = $('#new_password').val();
+            var confirm_pass = $('#confirm_password').val();
+            if (new_pass === confirm_pass) {
+                obj = {
+                    id: profile.id,
+                    password: $('#new_password').val(),
+                    type: 'pass'
+                }
+            } else {
+                swal('Error', 'New password and Confirm password is different!!', 'error');
+                isUpdate = false;
+            }
+        } else {
+            obj = {
+                id: profile.id,
+                full_name: $('#full_name').val(),
+                identity_id: $('#identity_id').val(),
+                day_of_birth: $('#day_of_birth').val(),
+                student_no: $('#student_no').val(),
+                phone_number: $('#phone_number').val(),
+                type: 'info'
+            };
+        }
+        if (isUpdate) {
+            $.ajax({
+                url: "/queue/api/student/updateProfile.php",
+                type: 'get',
+                cache: false,
+                dataType: 'text',
+                data: obj,
+                success: function () {
+                    swal({
+                            title: "Update Success",
+                            type: "success",
+                            showCancelButton: false,
+                            confirmButtonColor: "rgb(140, 212, 245)",
+                            confirmButtonText: "Yes",
+                            closeOnConfirm: true
+                        },
+                        function () {
+                            $('#new_password').val('');
+                            $('#confirm_password').val('');
+                            getUserById(user.id);
+                        });
+                },
+                error: function (error) {
+                    swal("error", error.responseText, "error");
+                }
+            });
+        }
+    });
+
+    var getDate = function (date) {
+        var dd = date.getDate();
+        var mm = date.getMonth() + 1; //January is 0!
+
+        var yyyy = date.getFullYear();
+        if (dd < 10) {
+            dd = '0' + dd;
+        }
+        if (mm < 10) {
+            mm = '0' + mm;
+        }
+        return dd + '/' + mm + '/' + yyyy;
+    };
+
+    var getUserById = function (id) {
+        $.ajax({
+            url: "/queue/api/student/getProfile.php?id=" + id,
+            cache: false,
+            success: function (data) {
+                profile = JSON.parse(data)[0];
+                var dob = new Date(profile.dob.substr(0, 10));
+                profile.dob = getDate(dob);
+                $('#user_name').val(profile.username);
+                $('#full_name').val(profile.fullname);
+                $('#day_of_birth').val(profile.dob);
+                $('#phone_number').val(profile.phone);
+                $('#student_no').val(profile.student_no);
+                $('#identity_id').val(profile.identity_id);
+            },
+            error: function (error) {
+                swal("error", error.responseText, "error");
+            }
+        });
+    };
+
+    getUserById(user.id);
 });
